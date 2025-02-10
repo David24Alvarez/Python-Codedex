7abeb757cf6a8dba4f2a454e3eb3ed4fbf157b44
David24Alvarez <140780046+David24Alvarez@users.noreply.github.com>
2025-02-10 18:28:47 -0400

    Add files via upload

---
+++ currency.py
@@ -0,0 +1,17 @@
+# Write code below 💖
+#solicitud de datos
+pesos_co = float(input("ingrese el total de pesos colombianos:"))
+soles_pe = float(input("ingrese el total de soles peruanos:"))
+reales_br = float(input("ingrese el total de reales brasileros:"))
+
+#calcular
+
+calculo1= pesos_co * 1 / 4126.39
+calculo2= soles_pe * 1 / 3.71
+calculo3= reales_br * 1 / 5.79
+
+#suma
+
+total = calculo1 + calculo2 + calculo3
+
+print(total)
