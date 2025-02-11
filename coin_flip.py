96d6d6c50b2dff4dcad8f08c22d15c339ce3b99a
David24Alvarez <140780046+David24Alvarez@users.noreply.github.com>
2025-02-11 18:52:45 -0400

    Add files via upload

---
+++ coin_flip.py
@@ -0,0 +1,9 @@
+#ejercicion de tira la moneda
+import random
+
+num = random.randint(0, 1)   # Generates a random number that's either 0 or 1
+
+if num > 0.5:
+  print('Heads')
+else:
+  print('Tails')
---
+++ grades.py
@@ -0,0 +1,9 @@
+# Write code below 
+grade = int(input("ingresa tu nota"))
+
+#comparaciones usando if/else
+
+if grade > 55:
+  print("haz aprobado.")
+else:
+  print("fallaste.")
