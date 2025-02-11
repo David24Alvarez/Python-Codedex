1fa792cd747033375a22368a9f436d2f7be6cd57
David24Alvarez <140780046+David24Alvarez@users.noreply.github.com>
2025-02-11 19:00:03 -0400

    Add files via upload

---
+++ ph_levels.py
@@ -0,0 +1,12 @@
+# Write code below 💖
+#se pide el valor al usuario
+ph = int(input("ingrese un valor: "))
+
+#se realiza la comparacion de los valores
+
+if ph >7:
+  print("Basico")
+elif ph < 7:
+  print("Acido")
+else:
+  print("Neutral")
